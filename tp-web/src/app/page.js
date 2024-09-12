b9ed3814819164293dff84996fe94a65aeeb10cb
Laps-F <maurolaps@gmail.com>
2024-09-12 12:47:01 -0300

    Adicionando HomePage

--- tp-web/src/app/page.js
+++ tp-web/src/app/page.js
@@ -1,101 +1,120 @@
-import Image from "next/image";
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation'
+
+import Decom from './assets/decom_logo.svg';
+import Ufop from './assets/ufop_logo.png';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const router = new useRouter();
+  const [searchQuery, setSearchQuery] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const predefinedSuggestions = ['cachorro', 'cacheado', 'cachoeira', 'cachimbo', 'cadeira', 'camisa'];
+
+  const handleSearchChange = (e) => {
+    const query = e.target.value;
+    setSearchQuery(query);
+
+    if (query.length > 0) {
+      const filteredSuggestions = predefinedSuggestions.filter((suggestion) =>
+        suggestion.toLowerCase().startsWith(query.toLowerCase())
+      );
+      setSuggestions(filteredSuggestions);
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(`Você pesquisou por: ${searchQuery}`);
+    setSuggestions([]);
+    router.push('/pesquisa')
+  };
+
+  const handleSuggestionClick = (suggestion) => {
+    setSearchQuery(suggestion);
+    setSuggestions([]);
+  };
+
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.title}>Buscador Científico</h1>
+
+      <form style={styles.searchContainer} onSubmit={handleSearch}>
+        <input 
+          type="text" 
+          placeholder="Digite sua pesquisa..." 
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+        <button type="submit">Pesquisar</button>
+
+        {suggestions.length > 0 && (
+          <ul style={styles.suggestionsList}>
+            {suggestions.map((suggestion, index) => (
+              <li 
+                key={index} 
+                onClick={() => handleSuggestionClick(suggestion)}
+                style={styles.suggestionItem}
+              >
+                {suggestion}
+              </li>
+            ))}
+          </ul>
+        )}
+      </form>
+
+      <div style={styles.sponsorsContainer}>
+        <img src={Decom.src} alt="Patrocinador 1" />
+        <img src={Ufop.src} alt="Patrocinador 2" />
+      </div>
     </div>
   );
 }
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    minHeight: '100vh', // Para ocupar a tela toda
+    position: 'relative', // Para que os patrocinadores fiquem absolutos no rodapé
+  },
+  searchContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: '220px',
+    position: 'relative', // Necessário para o posicionamento das sugestões
+    width: '100%',
+    maxWidth: '500px',
+  },
+  suggestionsList: {
+    position: 'absolute',
+    top: '110%', // Logo abaixo do campo de pesquisa
+    left: '0',
+    width: '100%', // Ocupa o mesmo espaço do input
+    backgroundColor: 'var(--secondary-text-color)',
+    border: '1px solid var(--border-color)',
+    borderRadius: '5px',
+    zIndex: 1,
+  },
+  suggestionItem: {
+    padding: '10px',
+    cursor: 'pointer',
+    borderBottom: '1px solid var(--highlight-color)',
+  },
+  sponsorsContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    gap: '20px',
+    position: 'absolute',
+    bottom: '20px', // Fixa o container no final da página
+    width: '100%',
+    maxWidth: '500px',
+  },
+};
---
+++ tp-web/src/app/pesquisa/page.js
@@ -0,0 +1,9 @@
+'use client'
+
+function Pesquisa() {
+    return (
+        <div>Tela de pesquisa</div>
+    );
+}
+
+export default Pesquisa;
