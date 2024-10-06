de40ed528f6d1a4e1d010df99be579974d83d994
ArthurCelestino <arthur.celestino@aluno.ufop.edu.br>
2024-10-06 20:28:53 -0300

    i hate merges

--- tp-web/src/app/components/StatsModal.js
+++ tp-web/src/app/components/StatsModal.js
@@ -1,36 +1,38 @@
 import React from 'react';
 import Modal from 'react-modal';
 
+import styles from './StatsModal.module.css';
+
 const StatsModal = ({ isOpen, onRequestClose, author, coAuthor}) => {
-  const styles = {
-    modal: {
-      content: {
-        display: 'flex',
-        flexDirection: 'row',
-        height: '100vh', // altura completa do modal
-        overflowX: 'scroll', // permite rolagem lateral se necessário
-        padding: '20px',
-      },
-      rect: {
-        width: '33.33%',
-        margin: '0 10px',
-        overflowY: 'scroll', // rolagem vertical em cada retângulo
-        border: '1px solid #ddd',
-        padding: '20px',
-        boxSizing: 'border-box',
-      },
-      closeButton: {
-        position: 'absolute',
-        top: '10px',
-        right: '10px',
-        cursor: 'pointer',
-      }
-    }
-  };
+//   const styles = {
+//     modal: {
+//       content: {
+//         display: 'flex',
+//         flexDirection: 'row',
+//         height: '100vh', // altura completa do modal
+//         overflowX: 'scroll', // permite rolagem lateral se necessário
+//         padding: '20px',
+//       },
+//       rect: {
+//         width: '33.33%',
+//         margin: '0 10px',
+//         overflowY: 'scroll', // rolagem vertical em cada retângulo
+//         border: '1px solid #ddd',
+//         padding: '20px',
+//         boxSizing: 'border-box',
+//       },
+//       closeButton: {
+//         position: 'absolute',
+//         top: '10px',
+//         right: '10px',
+//         cursor: 'pointer',
+//       }
+//     }
+//   };
 
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose} style={styles.modal}>
-      <div style={styles.modal.rect}>
+    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+      <div className={styles.modal.rect}>
         <h3>Instituições</h3>
         <ul>
           {author?.affiliations?.map((inst) => (
@@ -38,7 +40,7 @@ const StatsModal = ({ isOpen, onRequestClose, author, coAuthor}) => {
           )) || <p>Nenhuma instituição registrada</p>}
         </ul>
       </div>
-      <div style={styles.modal.rect}>
+      <div className={styles.modal.rect}>
         <h3>Tópicos</h3>
         <ul>
           {author?.topics?.map((topic, index) => (
@@ -46,7 +48,7 @@ const StatsModal = ({ isOpen, onRequestClose, author, coAuthor}) => {
           )) || <p>Nenhum tópico encontrado</p>}
         </ul>
       </div>
-      <div style={styles.modal.rect}>
+      <div className={styles.modal.rect}>
         <h3>Co-Autores</h3>
         <ul>
           {coAuthor?.map(co => (
@@ -54,7 +56,7 @@ const StatsModal = ({ isOpen, onRequestClose, author, coAuthor}) => {
           )) || <p>Nenhum co-autor encontrado</p>}
         </ul>
       </div>
-      <button style={styles.modal.closeButton} onClick={onRequestClose}>&times;</button>
+      <button className={styles.modal.closeButton} onClick={onRequestClose}>&times;</button>
     </Modal>
   );
 };
